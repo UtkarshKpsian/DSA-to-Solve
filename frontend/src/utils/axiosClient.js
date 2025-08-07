8271debd427b7bbdb0cb4808042623b4447e89f0
Utkarsh Mishra <mishrautkarsh319@gmail.com>
2025-08-07 22:04:37 +0530

    Update axiosClient to use environment variable for backend URL

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,13 +1,12 @@
 import axios from "axios"
 
-const axiosClient =  axios.create({
-    baseURL: 'http://localhost:3000',
-    withCredentials: true,
-    headers: {
-        'Content-Type': 'application/json'
-    }
+const axiosClient = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || 'https://backend-project-7vsd.onrender.com',
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json'
+  }
 });
-
 
 export default axiosClient;
 
